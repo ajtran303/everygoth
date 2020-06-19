8a5fa00f3d8fd2b02c4f97c0d881f1cca885cde7
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-19 07:16:49 -0600

    Add assertion that no dictionary words contain spaces (they are all one-word entries)

--- test/dictionary_reader_test.rb
+++ test/dictionary_reader_test.rb
@@ -4,26 +4,25 @@ require "./lib/dictionary_reader"
 
 class DictionaryReaderTest < MiniTest::Test
 
+  @@dictionary_reader = DictionaryReader.new
+
   def test_it_exists_with_attributes
-    dictionary_reader = DictionaryReader.new
-    assert_instance_of DictionaryReader, dictionary_reader
+    assert_instance_of DictionaryReader, @@dictionary_reader
   end
 
-  def test_it_has_a_word_list
-    dictionary_reader = DictionaryReader.new
+  def test_it_has_a_words
+    words = @@dictionary_reader.word_list
 
-    word_list = dictionary_reader.word_list
-
-    assert_instance_of Array, word_list
-    assert_equal 235_886, word_list.size
-    assert_equal "A", word_list.first
-    assert_equal "Zyzzogeton", word_list.last
+    assert_instance_of Array, words
+    assert_equal 235_886, words.size
+    assert_equal "A", words.first
+    assert_equal "Zyzzogeton", words.last
+    assert_equal true, words.none? { |word| word.chars.include? " " }
   end
 
   def test_it_can_find_the_next_word
-    dictionary_reader = DictionaryReader.new
-    assert_equal "a", dictionary_reader.find_next_word("A")
-    assert_nil dictionary_reader.find_next_word("Zyzzogeton")
+    assert_equal "a", @@dictionary_reader.find_next_word("A")
+    assert_nil @@dictionary_reader.find_next_word("Zyzzogeton")
   end
 
   def test_its_class_can_find_next_word
