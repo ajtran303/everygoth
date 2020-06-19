c8d3de1e298153780ac729b8c90678993470ec4b
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-19 07:25:14 -0600

    Add assertions that Gothify can work with both words and phrases

--- test/gothify_test.rb
+++ test/gothify_test.rb
@@ -10,11 +10,15 @@ class GothifyModuleTest < MiniTest::Test
   end
 
   def test_it_can_prepend_goth
+    assert_equal "goth hello", @gothified_object.gothify("hello")
     assert_equal "goth hello world", @gothified_object.gothify("hello world")
+    assert_equal "goth good-bye", @gothified_object.gothify("good-bye")
   end
 
   def test_it_can_remove_goth_prefix
+    assert_equal "hello", @gothified_object.ungothify("goth hello")
     assert_equal "hello world", @gothified_object.ungothify("goth hello world")
+    assert_equal "good-bye", @gothified_object.ungothify("goth good-bye")
   end
 
 end
